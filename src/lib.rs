ac065422f61a663f067458fa683c76e2a0b32384
hatakesoft <hermit183@outlook.com>
2025-11-05 10:36:02 +0900

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
 use wasm_bindgen::prelude::*;
+use web_sys::CanvasRenderingContext2d;
 
-// JavaScriptから呼び出せる関数
 #[wasm_bindgen]
-pub fn greet(name: &str) -> String {
-    format!("Hello, {}!", name)
+pub fn draw_circle() {
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document.get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
+
+    let ctx = canvas
+        .get_context("2d").unwrap()
+        .unwrap()
+        .dyn_into::<CanvasRenderingContext2d>()
+        .unwrap();
+    
+    ctx.begin_path();
+    ctx.arc(150.0, 75.0, 50.0, 0.0, std::f64::consts::PI * 2.0).unwrap();
+    ctx.set_fill_style(&JsValue::from_str("blue"));
+    ctx.fill();
 }
